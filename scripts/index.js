3d3c224df993a6caeab2343ea58eb0804655500e
AEMxr <asmalcuit@outlook.com>
2024-07-19 00:22:11 -0700

    .js changes: lets to const, condense functions, layout/ readability, comment removal

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,3 @@
-// open and close modal events
-const modal = document.getElementById("modalEdit");
-const modalOpen = document.getElementById("editButton");
-const modalClose = document.getElementsByClassName("modal__close")[0];
-
-modalOpen.addEventListener("click", function (event) {
-  modal.style.display = "block";
-});
-
-modalClose.addEventListener("click", function (event) {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
-
-//***Save submit button event***
-
-// find the form in the DOM
-const profileForm = document.querySelector(".modal__form");
-
-// find the form fields in the DOM
-const nameInput = document.getElementById("name");
-const jobInput = document.getElementById("description");
-
-// find the profile elements in the DOM
-const profileName = document.querySelector(".profile__name");
-const profileJob = document.querySelector(".profile__profession");
-
-// the form submission handler. Note that its name
-// starts with a verb and concisely describes what it does
-function saveChanges(evt) {
-  evt.preventDefault();
-
-  // get the values of each field from the value property
-  // of the corresponding input element
-  const newName = nameInput.value;
-  const newJob = jobInput.value;
-
-  // insert new values into the textContent property of the
-  // corresponding profile elements
-  profileName.textContent = newName;
-  profileJob.textContent = newJob;
-
-  // Close the modal
-  modal.style.display = "none";
-}
-
-// connect the handler to the form:
-// it will watch the submit event
-profileForm.addEventListener("submit", saveChanges);
-
-//*** Card template clone function and heart button function ***
 // card data array
 const initialCards = [
   {
@@ -82,59 +26,81 @@ const initialCards = [
   },
 ];
 
+//card clone template
 const userTemplate = document.querySelector("#card-template").content;
 const cardsContainer = document.querySelector(".cards__grid");
 
+//Save submit button event
+const profileForm = document.querySelector(".modal__form");
+const nameInput = document.getElementById("name");
+const jobInput = document.getElementById("description");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__profession");
+
+// open and close modal events
+const modal = document.getElementById("modalEdit");
+const modalOpen = document.getElementById("editButton");
+const modalClose = document.getElementsByClassName("modal__close")[0];
+
+function openProfileModal() {
+  modalOpen.addEventListener("click", () => {
+    modal.classList.add("modal_opened");
+  });
+}
+
+function closeProfileModal() {
+  modalClose.addEventListener("click", () => {
+    modal.classList.remove("modal_opened");
+  });
+}
+
+// modalOpen.addEventListener("click", () => {
+//   modal.classList.add("modal_opened");
+// });
+
+// modalClose.addEventListener("click", () => {
+//   modal.classList.remove("modal_opened");
+// });
+
+// the form submission handler
+function saveChanges(evt) {
+  evt.preventDefault();
+
+  const newName = nameInput.value;
+  const newJob = jobInput.value;
+
+  profileName.textContent = newName;
+  profileJob.textContent = newJob;
+
+  modal.style.display = "none";
+}
+
+// connect the handler to the form
+profileForm.addEventListener("submit", saveChanges);
+
+//template clone and card__heart like button
 function getCardElement(data) {
-  // Clone template with all its content and store it into a card element variable
-  let userElement = userTemplate.cloneNode(true);
+  const userElement = userTemplate.cloneNode(true);
 
-  // Access card title and image and store in variables
-  let cardImage = userElement.querySelector(".card__image-size");
-  let cardLabel = userElement.querySelector(".card__label");
-  let cardHeart = userElement.querySelector(".card__heart");
+  const cardImage = userElement.querySelector(".card__image-size");
+  const cardLabel = userElement.querySelector(".card__label");
+  const cardHeart = userElement.querySelector(".card__heart");
 
-  // Check if cardImage and cardLabel are correctly selected
-  if (!cardImage || !cardLabel || !cardHeart) {
-    console.error(
-      "Template cloning failed: cardImage or cardLabel is not defined"
-    );
-    return null;
-  }
-
-  // Set path to image to the link field
   cardImage.src = data.link;
-
-  // Set the image alt text to name field
   cardImage.alt = data.name;
-
-  // Set card title to name field
   cardLabel.textContent = data.name;
 
-  cardHeart.addEventListener("click", function () {
-    if (
-      cardHeart.style.backgroundImage.includes(
-        "../images/vector-imgs/heart.svg"
-      )
-    ) {
-      cardHeart.style.backgroundImage =
-        'url("../images/vector-imgs/heartFilled.svg")';
-    } else {
-      cardHeart.style.backgroundImage =
-        'url("../images/vector-imgs/heart.svg")';
-    }
+  cardHeart.addEventListener("click", () => {
+    cardHeart.classList.toggle("card__heart_active");
   });
 
-  // Return the HTML element with filled data
   return userElement;
 }
 
 // Iterate the cards array using forEach with a function expression
-initialCards.forEach(function (cardData) {
-  // Run getCardElement() function on the card object to create the HTML element
-  let cardElement = getCardElement(cardData);
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
 
-  // Log card element
   console.log("Appending card element:", cardElement);
 
   // Ensure the returned element is a node before appending
