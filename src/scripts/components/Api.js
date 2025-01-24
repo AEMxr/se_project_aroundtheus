22716b84506e59fce5ec65b5987e9b8359a48f0b
AEMxr <asmalcuit@outlook.com>
2025-01-23 18:41:05 -0800

    even DRYer api

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -4,6 +4,25 @@ export default class Api {
     this._headers = headers;
   }
 
+  _getRequestConfig(method, body = null) {
+    const config = {
+      method,
+      headers: this._headers,
+    };
+    if (body) {
+      config.body = JSON.stringify(body);
+    }
+    return config;
+  }
+
+  _getEndpoint(type = "cards", id = "", action = "") {
+    const base = `${this._baseUrl}/${type}`;
+    if (!id) return base;
+    const path = `${base}/${id}`;
+    if (!action) return path;
+    return `${path}/${action}`;
+  }
+
   async _handleServerResponse(res) {
     if (res.ok) {
       return res.json();
@@ -12,75 +31,69 @@ export default class Api {
   }
 
   async patchAvatar(avatarData) {
-    const res = await fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar: avatarData.avatar,
-      }),
-    });
+    const res = await fetch(
+      this._getEndpoint("users/me/avatar"),
+      this._getRequestConfig("PATCH", { avatar: avatarData.avatar })
+    );
     return this._handleServerResponse(res);
   }
 
   async getUserInformation() {
-    const res = await fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    });
+    const res = await fetch(
+      this._getEndpoint("users/me"),
+      this._getRequestConfig("GET")
+    );
     return this._handleServerResponse(res);
   }
 
   async patchUserInformation(userData) {
-    const res = await fetch(`${this._baseUrl}/users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
+    const res = await fetch(
+      this._getEndpoint("users/me"),
+      this._getRequestConfig("PATCH", {
         name: userData.name,
         about: userData.about,
-      }),
-    });
+      })
+    );
     return this._handleServerResponse(res);
   }
 
   async getInitialCards() {
-    const res = await fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    });
+    const res = await fetch(this._getEndpoint(), this._getRequestConfig("GET"));
     return this._handleServerResponse(res);
   }
 
   async postNewCard(cardData) {
-    const res = await fetch(`${this._baseUrl}/cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({
+    const res = await fetch(
+      this._getEndpoint(),
+      this._getRequestConfig("POST", {
         name: cardData.name,
         link: cardData.link,
-      }),
-    });
+      })
+    );
     return this._handleServerResponse(res);
   }
 
   async deleteCard(cardId) {
-    const res = await fetch(`${this._baseUrl}/cards/${cardId}`, {
-      method: "DELETE",
-      headers: this._headers,
-    });
+    const res = await fetch(
+      this._getEndpoint("cards", cardId),
+      this._getRequestConfig("DELETE")
+    );
     return this._handleServerResponse(res);
   }
 
   async addLike(cardId) {
-    const res = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      method: "PUT",
-      headers: this._headers,
-    });
+    const res = await fetch(
+      this._getEndpoint("cards", cardId, "likes"),
+      this._getRequestConfig("PUT")
+    );
     return this._handleServerResponse(res);
   }
 
   async removeLike(cardId) {
-    const res = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      method: "DELETE",
-      headers: this._headers,
-    });
+    const res = await fetch(
+      this._getEndpoint("cards", cardId, "likes"),
+      this._getRequestConfig("DELETE")
+    );
     return this._handleServerResponse(res);
   }
 }
