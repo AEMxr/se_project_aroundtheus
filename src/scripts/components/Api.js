3e8c7195a9c86f553415c4108a9fe88a828c5504
AEMxr <asmalcuit@outlook.com>
2025-01-24 13:34:14 -0800

    toggle like api

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -4,7 +4,7 @@ export default class Api {
     this._headers = headers;
   }
 
-  _getRequestConfig(method, body = null) {
+  _getRequestConfig(method = "GET", body = null) {
     const config = {
       method,
       headers: this._headers,
@@ -23,6 +23,15 @@ export default class Api {
     return `${path}/${action}`;
   }
 
+  _getUserEndpoint(action = "") {
+    const base = `${this._baseUrl}/users/me`;
+    return action ? `${base}/${action}` : base;
+  }
+
+  _getCardEndpoint(id = "", action = "") {
+    return this._getEndpoint("cards", id, action);
+  }
+
   async _handleServerResponse(res) {
     if (res.ok) {
       return res.json();
@@ -30,25 +39,31 @@ export default class Api {
     throw new Error(`Error: ${res.status}`);
   }
 
+  async toggleLike(cardId, isLiking) {
+    const method = isLiking ? "PUT" : "DELETE";
+    const res = await fetch(
+      this._getCardEndpoint(cardId, "likes"),
+      this._getRequestConfig(method)
+    );
+    return this._handleServerResponse(res);
+  }
+
   async patchAvatar(avatarData) {
     const res = await fetch(
-      this._getEndpoint("users/me/avatar"),
+      this._getUserEndpoint("avatar"),
       this._getRequestConfig("PATCH", { avatar: avatarData.avatar })
     );
     return this._handleServerResponse(res);
   }
 
   async getUserInformation() {
-    const res = await fetch(
-      this._getEndpoint("users/me"),
-      this._getRequestConfig("GET")
-    );
+    const res = await fetch(this._getUserEndpoint(), this._getRequestConfig());
     return this._handleServerResponse(res);
   }
 
   async patchUserInformation(userData) {
     const res = await fetch(
-      this._getEndpoint("users/me"),
+      this._getEndpoint(),
       this._getRequestConfig("PATCH", {
         name: userData.name,
         about: userData.about,
@@ -58,7 +73,7 @@ export default class Api {
   }
 
   async getInitialCards() {
-    const res = await fetch(this._getEndpoint(), this._getRequestConfig("GET"));
+    const res = await fetch(this._getEndpoint(), this._getRequestConfig());
     return this._handleServerResponse(res);
   }
 
@@ -76,22 +91,6 @@ export default class Api {
   async deleteCard(cardId) {
     const res = await fetch(
       this._getEndpoint("cards", cardId),
-      this._getRequestConfig("DELETE")
-    );
-    return this._handleServerResponse(res);
-  }
-
-  async addLike(cardId) {
-    const res = await fetch(
-      this._getEndpoint("cards", cardId, "likes"),
-      this._getRequestConfig("PUT")
-    );
-    return this._handleServerResponse(res);
-  }
-
-  async removeLike(cardId) {
-    const res = await fetch(
-      this._getEndpoint("cards", cardId, "likes"),
       this._getRequestConfig("DELETE")
     );
     return this._handleServerResponse(res);
