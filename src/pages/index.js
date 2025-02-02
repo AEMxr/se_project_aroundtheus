26639b9d795fbd9a8e97c93bda47362496a6b45d
AEMxr <asmalcuit@outlook.com>
2025-02-02 11:13:48 -0800

    Updates

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-import App from "../scripts/components/App.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const app = new App();
-  app.initialize();
-});
--- src/scripts/pages/index.js
+++
@@ -1,8 +0,0 @@
-function createCard(item) {
-  const card = new Card(item, cardSelector, handleImageClick);
-  return card.getCardElement();
-}
-
-// For new card creation
-const cardElement = createCard(newCardData);
-cardSection.addItem(cardElement);
